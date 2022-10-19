be97485b3caa4c370c2bc97555acc073c2fbaebb
karelbondan <karelbondan@gmail.com>
2022-10-20 00:07:26 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Footer from '../components/footer'
+import Navbar from '../components/navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
 }
 
 export default MyApp
